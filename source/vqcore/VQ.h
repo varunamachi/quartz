1f2d37bcc2387d616fac07d76dcd8d49a47da8b0
Varuna Amachi <varunamachi@gmail.com>
2016-05-31 19:09:30 +0530

    Changed macros

--- source/vqcore/VQ.h
+++ source/vqcore/VQ.h
@@ -1,11 +1,11 @@
 #pragma once
 
 //#if defined( VQ_CORE )
-// #define VQ_CORE_EXPORT Q_DECL_EXPORT
+// #define VQ_API Q_DECL_EXPORT
 //#else
-// #define VQ_CORE_EXPORT Q_DECL_IMPORT
+// #define VQ_API Q_DECL_IMPORT
 //#endif
 
-#define VQ_CORE_EXPORT
+#define VQ_API
 
 
