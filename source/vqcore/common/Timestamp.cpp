85593ca9c14d277ef014434e5efc95ab633d63ed
Varuna Amachi <varunamachi@gmail.com>
2016-06-07 18:58:55 +0530

    Added empty impl

--- source/vqcore/common/Timestamp.cpp
+++ source/vqcore/common/Timestamp.cpp
@@ -4,4 +4,106 @@
 
 namespace Vq {
 
+
+Timestamp::Timestamp( const TimeVal &timeVal ) {
+
+}
+
+
+
+Timestamp::Timestamp( const Timestamp &other ) {
+
+}
+
+
+
+Timestamp::Timestamp( Timestamp &&other ) {
+
+}
+
+
+Timestamp & Timestamp::operator = ( const Timestamp &other ) {
+
+}
+
+
+Timestamp & Timestamp::operator = ( const Timestamp &&other ) {
+
+}
+
+
+bool Timestamp::operator == ( const Timestamp &other ) {
+
+}
+
+
+bool Timestamp::operator != ( const Timestamp &other ) {
+
+}
+
+
+bool Timestamp::operator >  ( const Timestamp &other ) {
+
+}
+
+
+bool Timestamp::operator >= ( const Timestamp &other ) {
+
+}
+
+
+bool Timestamp::operator <  ( const Timestamp &other ) {
+
+}
+
+
+bool Timestamp::operator <= ( const Timestamp &other ) {
+
+}
+
+
+Timestamp Timestamp::operator + ( const Timestamp &other ) {
+
+}
+
+
+Timestamp Timestamp::operator - ( const Timestamp &other ) {
+
+}
+
+
+Timestamp & Timestamp::operator += ( const Timestamp &other ) {
+
+}
+
+
+Timestamp & Timestamp::operator -= ( const Timestamp &other ) {
+
+}
+
+
+Timestamp::TimeVal Timestamp::toUTCMicroSec() const  {
+
+}
+
+
+Timestamp::TimeVal Timestamp::toPosixMicroSec() const  {
+
+}
+
+
+Timestamp Timestamp::now() {
+
+}
+
+
+Timestamp Timestamp::fromPosixEpoch( const std::time_t &time ) {
+
+}
+
+
+Timestamp Timestamp::fromUTCTime( const TimeVal &utc ) {
+
+}
+
 }
