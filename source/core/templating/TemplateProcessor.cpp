7db77f678691d59a0fd88216de62cd93fcdb32da
Varuna Amachi <varunamachi@gmail.com>
2017-03-22 19:14:32 +0530

    [WIP] iniitial template process impl

--- source/core/templating/TemplateProcessor.cpp
+++ source/core/templating/TemplateProcessor.cpp
@@ -55,10 +55,88 @@ bool TemplateProcessor::process( const QString &inputPath,
 
 }
 
-bool TemplateProcessor::process(QTextStream &input, QTextStream &output)
+bool TemplateProcessor::process( QTextStream &input, QTextStream &output )
 {
+    auto tmpl = input.readAll();
+    if( tmpl.isEmpty() ) {
+        return true;
+    }
     bool result = false;
-
+    auto cursor = 0;
+//    auto size = ( tmpt.size() / 3 ) * 3;
+//    bool replacing = false;
+//    while( cursor < size ) {
+//        auto val1 = tmpt[ cursor ++ ];
+//        auto val2 = tmpt[ cursor ++ ];
+//        auto val3 = tmpt[ cursor ++ ];
+//        if( val1 == '$' && val2 == '<' && val3 == '<' ) {
+//            replacing = true;
+//            //do template processing
+//        }
+//        else {
+//            output << val1 << val2 << val3;
+//        }
+//    }
+//    auto remaining = tmpt.size() - size;
+//    if( remaining != 0 ) {
+//        if( remaining == 2
+//                && tmpt[ cursor ] == '>'
+//                && tmpt[ cursor + 1 ] == '>'
+//                && replacing ) {
+//            //ignore
+//        }
+//        else {
+//            output <<
+//        }
+//    }
+    auto matchCount = 0;
+    auto inMatch = false;
+    while( cursor < tmpl.size() ) {
+        auto token = tmpl[ cursor ];
+        if( matchCount == 0 && token == '$' ) {
+            ++ matchCount;
+        }
+        else if( matchCount == 1 && token == '<' ) {
+            ++ matchCount;
+            inMatch = true;
+        }
+        else {
+            if( matchCount == 1 ) {
+                //because I had eaten doller in the first if
+                output << '$';
+                matchCount = 0;
+            }
+            output << token;
+        }
+        ++ cursor;
+        if( inMatch ) {
+            matchCount = 0;
+            auto unmatchCount = 0;
+            QString key;
+            QTextStream stream{ &key };
+            while( inMatch && cursor < tmpl.size() ) {
+                token = tmpl[ cursor ];
+                if( unmatchCount == 0 && token == '>' ) {
+                    ++ unmatchCount;
+                }
+                else if( unmatchCount == 1 && token == '$' ) {
+                    stream.flush();
+                    auto def = "$<" + key + ">$";
+                    auto var = m_data->m_variables.value( key, def );
+                    output << var;
+                    inMatch = false;
+                }
+                else {
+                    if( unmatchCount == 1 ) {
+                        stream << '$';
+                        unmatchCount = 0;
+                    }
+                    stream << token;
+                }
+                ++ cursor;
+            }
+        }
+    }
     return result;
 }
 
