e739fe2bb44bcd96dd67b355f212c7f594515e77
Varuna Amachi <varunamachi@gmail.com>
2017-05-16 18:19:00 +0530

    Singe parsing logic to parse for loops - completed

--- source/common/templating/AdvancedTemplateProcessor.cpp
+++ source/common/templating/AdvancedTemplateProcessor.cpp
@@ -11,8 +11,7 @@ namespace Quartz {
 const QString ADV_TOKEN_FIRST = QString{ "$" };
 const QString ADV_TOKEN_SEC = QString{ "[" };
 const QString ADV_FOREACH_DELEM = QString{ ":>" };
-const QString ADV_FOREACH_KW = QString{ "FOREACH" };
-const QString ADV_FOR_KW = QString{ "FOREACH" };
+const QString ADV_FOR_KW = QString{ "FOR" };
 const QString ADV_IF_KW = QString{ "IF" };
 const QString ADV_FOR_IN_KW = QString{ "IN" };
 
@@ -93,10 +92,7 @@ QString AdvancedTemplateProcessor::processBlocks( const QStringRef &input ) {
                 }
                 else if( unmatchCount == 1 && token == ADV_TOKEN_FIRST ) {
                     auto block = input.mid( blockStart, cursor - 2 );
-                    if( block.startsWith( ADV_FOREACH_KW )) {
-                        result = processForeach( *block.string() );
-                    }
-                    else if( block.startsWith( ADV_IF_KW )) {
+                    if( block.startsWith( ADV_IF_KW )) {
                         result = processIf( *block.string() );
                     }
                     else if( block.startsWith( ADV_FOR_KW )) {
@@ -117,108 +113,163 @@ QString AdvancedTemplateProcessor::processBlocks( const QStringRef &input ) {
     return result;
 }
 
-QString AdvancedTemplateProcessor::processForeach( const QString &input )
+//QString AdvancedTemplateProcessor::processForeach( const QString &input )
+//{
+//    //To support loop like:
+//    //$[forach val in list :>
+//    //  cout << val;
+//    //]$
+
+//    auto segments = input.split( ADV_FOREACH_DELEM );
+//    if( segments.size() != 2 ) {
+//        QZ_ERROR( "Qz:Cmn:Tmpl" ) << "Malformed foreach loop found";
+//        return input;
+//    }
+
+//    auto validate = []( const QStringList &tokens ) -> bool {
+//        return tokens.size() == 4
+//                && tokens.at( 0 ) == ADV_FOREACH_KW
+//                && tokens.at( 2 ) == ADV_FOR_IN_KW;
+//    };
+
+
+//    auto forTokens = segments.at( 0 ).split( QRegExp("\\s+"),
+//                                             QString::SkipEmptyParts );
+//    if( validate( forTokens )) {
+//        QString varValue{};
+//        auto varName = forTokens.at( 1 );
+//        auto list = var( forTokens.at( 3 )).toStringList();
+//        auto provider = [ & ]( const QString &key,
+//                               const QString &def ) -> QString {
+//            if( key == varName ) {
+//                return varValue;
+//            }
+//            auto var = this->var( key );
+//            if( ! var.isValid() ) {
+//                return this->toString( var );
+//            }
+//            return def;
+//        };
+//        QString block;
+//        QTextStream stream{ &block };
+//        foreach( const QString &item, list ) {
+//            varValue = item;
+//            TemplateProcessor::process( input, stream, provider );
+//        }
+//        return  block;
+//    }
+//    else {
+//        //print errore
+//    }
+//    return input;
+//}
+
+static bool check( const QString &input,
+                   const QString &token,
+                   QZ_IN_OUT int &cursor )
 {
-    //To support loop like:
-    //$[forach val in list :>
-    //  cout << val;
-    //]$
-
-    auto segments = input.split( ADV_FOREACH_DELEM );
-    if( segments.size() != 2 ) {
-        QZ_ERROR( "Qz:Cmn:Tmpl" ) << "Malformed foreach loop found";
-        return input;
-    }
-
-    auto validate = []( const QStringList &tokens ) -> bool {
-        return tokens.size() == 4
-                && tokens.at( 0 ) == ADV_FOREACH_KW
-                && tokens.at( 2 ) == ADV_FOR_IN_KW;
-    };
-
-
-    auto forTokens = segments.at( 0 ).split( QRegExp("\\s+"),
-                                             QString::SkipEmptyParts );
-    if( validate( forTokens )) {
-        QString varValue{};
-        auto varName = forTokens.at( 1 );
-        auto list = var( forTokens.at( 3 )).toStringList();
-        auto provider = [ & ]( const QString &key,
-                               const QString &def ) -> QString {
-            if( key == varName ) {
-                return varValue;
-            }
-            auto var = this->var( key );
-            if( ! var.isValid() ) {
-                return this->toString( var );
-            }
-            return def;
-        };
-        QString block;
-        QTextStream stream{ &block };
-        foreach( const QString &item, list ) {
-            varValue = item;
-            TemplateProcessor::process( input, stream, provider );
+    bool result = true;
+    auto dupCursor = cursor;
+    for( auto i = 0; i < token.size(); ++ i, ++ dupCursor ) {
+        if( dupCursor >= input.size()
+                || input[ dupCursor ] != token[ i ]) {
+            result = false;
+            break;
         }
-        return  block;
     }
-    else {
-        //print errore
+    if( result ) {
+        cursor = cursor + token.size();
     }
-    return input;
+    return result;
 }
 
+static inline bool checkWhitespace( const QString &input,
+                                    QZ_IN_OUT int &cursor )
+{
+    auto result = false;
+    auto ch = input.at( cursor );
+    if( ch == ' '
+            || ch == '\t'
+            || ch == '\n'
+            || ch == '\r' ) {
+        ++ cursor;
+        result = true;
+    }
+    return result;
+}
+
+struct Range
+{
+    Range( int start, int end, int increment )
+        : m_start{ start }
+        , m_end{ end }
+        , m_inc{ increment }
+    {
+        m_valid = ( m_start != m_end )
+                && (( m_start < m_end  &&  m_inc > 0 )
+                || ( m_start > m_end  &&  m_inc < 0 ));
+    }
+
+    Range()
+        : m_start{ 0 }
+        , m_end{ 0 }
+        , m_inc{ 0 }
+        , m_valid{ false }
+    {
+
+    }
+
+
+    int m_start;
+
+    int m_end;
+
+    int m_inc;
+
+    bool m_valid;
+};
+
+Range parseRange( const QString &range )
+{
+    auto rg = range.mid( 4, ( range.size() - 2 - 4 ));
+    auto parts = rg.split( "," );
+    Range result{};
+    if( parts.size() == 2 || parts.size() == 3 ) {
+        auto startOk = true;
+        auto endOk = true;
+        auto incOk = true;
+        auto start = parts.at( 0 ).trimmed().toInt( &startOk );
+        auto end = parts.at( 1 ).trimmed().toInt( &endOk );
+        auto inc = 1;
+        if( parts.size() > 2 ) {
+            inc = parts.at( 2 ).trimmed().toInt( &incOk );
+        }
+        if( startOk && endOk && incOk ) {
+            result = Range{ start, end, inc };
+        }
+    }
+    return result;
+}
 
 QString AdvancedTemplateProcessor::processFor( const QString &input )
 {
-    //To support loops like:
-    //$[for i in range(1, 100)]$
-    //Not implemented yet
+    //For parsing for-each and for-range loop and expanding the content
     auto cursor = 0;
-    auto check = [ & ]( const QString &token ) -> bool {
-        bool result = true;
-        auto dupCursor = cursor;
-        for( auto i = 0; i < token.size(); ++ i, ++ dupCursor ) {
-            if( dupCursor >= input.size()
-                    || input[ dupCursor ] != token[ i ]) {
-                result = false;
-                break;
-            }
-        }
-        if( result ) {
-            cursor = cursor + token.size();
-        }
-        return result;
-    };
-
-    auto checkWhitespace = [ & ] () -> bool {
-        auto result = false;
-        auto ch = input.at( cursor );
-        if( ch == ' '
-                || ch == '\t'
-                || ch == '\n'
-                || ch == '\r' ) {
-            ++ cursor;
-            result = true;
-        }
-        return result;
-    };
-
-
     auto tokenStart = 0;
     auto tokenNumber = 0;
     QString content;
     QString varName;
     QString listName;
+    Range range;
     while( cursor <= input.size() ) {
-        if( checkWhitespace()
-                || check( ":>" )
+        if( checkWhitespace( input, cursor )
+                || check( input, ":>", cursor )
                 || cursor == input.size() ) {
             auto num = cursor - tokenStart - 1;
             if( num != 0 ) {
                 auto token = input.mid( tokenStart, num );
                 ++ cursor;
-                if( tokenNumber == 0 && token == ADV_FOREACH_KW ) {
+                if( tokenNumber == 0 && token == ADV_FOR_KW ) {
                     //expected
                     continue;
                 }
@@ -232,7 +283,12 @@ QString AdvancedTemplateProcessor::processFor( const QString &input )
                 else if( tokenNumber == 3 ) {
                     auto list = token;
                     if( list.startsWith( "range" )) {
-                        //parse range
+                        range = parseRange( list );
+                        if( ! range.m_valid ) {
+                            QZ_ERROR( "Qz:Cmn:Tmpl" )
+                                    << "Failed to parse range of for loop";
+                            break;
+                        }
                     }
                     else {
                         listName = token;
@@ -248,6 +304,41 @@ QString AdvancedTemplateProcessor::processFor( const QString &input )
         else {
             tokenStart = cursor;
         }
+    }
+    //Now expand the content if its not empty
+    if( ! content.isEmpty() ) {
+        QString value = "";
+        auto provider = [ & ]( const QString &key,
+                               const QString &def ) -> QString {
+            if( key == varName ) {
+                return value;
+            }
+            auto var = this->var( key );
+            if( ! var.isValid() ) {
+                return this->toString( var );
+            }
+            return def;
+        };
+        QString output;
+        QTextStream stream{ &output };
+        if( range.m_valid ) {
+            for( auto i = range.m_start; i < range.m_end; i += range.m_inc ) {
+                value = QString::number( i );
+                TemplateProcessor::process( input, stream, provider );
+                stream << '\n';
+            }
+        }
+        else {
+            auto list = var( listName ).toStringList();
+            foreach( const QString &item, list ) {
+                value = item;
+                TemplateProcessor::process( input, stream, provider );
+                stream << '\n';
+            }
+        }
+    }
+    else {
+        QZ_ERROR( "Qz:Cmn:Tmpl" ) << "Failed to parse for loop";
     }
     return QString{};
 }
--- source/common/templating/AdvancedTemplateProcessor.h
+++ source/common/templating/AdvancedTemplateProcessor.h
@@ -16,7 +16,7 @@ public:
     bool process( QString &input, QTextStream &output ) override;
 
 private:
-    QString processForeach( const QString &input );
+//    QString processForeach( const QString &input );
 
     QString processFor( const QString &input );
 
