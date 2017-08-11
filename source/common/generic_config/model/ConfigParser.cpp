2536156042eddfa4acc4636826c522d6dd6b9ee2
Varun Amachi <varunamachi@gmail.com>
2017-08-11 19:48:38 +0530

    Added beginning of a generic config parser

--- source/common/generic_config/model/ConfigParser.cpp
+++ source/common/generic_config/model/ConfigParser.cpp
@@ -1,10 +1,41 @@
-
 #include <QByteArray>
+#include <QDomElement>
+#include <QDomNodeList>
+#include <QDomNode>
+#include <QDomNamedNodeMap>
 
 #include "ConfigParser.h"
 #include "Config.h"
+#include "Param.h"
+#include "BooleanParam.h"
+#include "ChoiceParam.h"
+#include "RangeParam.h"
+#include "TextParam.h"
 
 namespace Quartz {
+
+ParamType toParamType( const QString &strType )
+{
+    if( strType == "boolean" ) {
+        return ParamType::Boolean;
+    }
+    else if( strType == "choice" ) {
+        return ParamType::Choice;
+    }
+    else if( strType == "range" ) {
+        return ParamType::Range;
+    }
+    return ParamType::Text;
+}
+
+QString nodeValue( const QDomNamedNodeMap &attr, const QString &key )
+{
+    auto domNode = attr.namedItem( key );
+    if( ! domNode.isNull() ) {
+        return domNode.nodeValue();
+    }
+    return QString{};
+}
 
 std::unique_ptr< Config > ConfigParser::parse( const QByteArray &content )
 {
@@ -14,6 +45,60 @@ std::unique_ptr< Config > ConfigParser::parse( const QByteArray &content )
     std::unique_ptr< Config > config;
 
     return config;
+}
+
+std::unique_ptr< Config > ConfigParser::parse( const QDomElement &configRoot )
+{
+    std::unique_ptr< Config > config;
+    auto paramList = configRoot.elementsByTagName( "param" );
+    for( auto i = 0; i < paramList.size(); ++ i ) {
+        auto paramNode = paramList.at( i );
+        auto param = parseParam( paramNode.toElement() );
+        if( param != nullptr ) {
+
+        }
+        else {
+            //log error
+            //continue
+        }
+
+    }
+    return config;
+}
+
+std::shared_ptr< Param > ConfigParser::parseParam(
+        const QDomElement &paramNode )
+{
+    std::shared_ptr< Param > param;
+    auto attr       = paramNode.attributes();
+    auto strType    = nodeValue( attr, "type" );
+    auto id         = nodeValue( attr, "id" );
+    auto name       = nodeValue( attr, "name" );
+    auto desc       = nodeValue( attr, "description" );
+    auto type       = toParamType( strType );
+    if( type == ParamType::Boolean ) {
+        auto strDef = nodeValue( attr, "default" );
+        auto def = QString::compare( strDef, "true", Qt::CaseInsensitive );
+        auto bparam = std::make_shared< BooleanParam >( id, name, desc );
+        bparam->setDefaultValue( def );
+    }
+    else if( type == ParamType::Choice ) {
+
+    }
+    else if( type == ParamType::Range ) {
+
+    }
+    else if( type == ParamType::Text ) {
+
+    }
+    return param;
+}
+
+std::shared_ptr<Group> ConfigParser::parseGroup( const QDomElement &groupNode )
+{
+    std::shared_ptr< Group > group;
+
+    return  group;
 }
 
 
--- source/common/generic_config/model/ConfigParser.h
+++ source/common/generic_config/model/ConfigParser.h
@@ -3,19 +3,25 @@
 #include <memory>
 
 class QByteArray;
+class QDomElement;
 
 namespace Quartz {
 
 class Config;
+class Param;
+class Param;
+class Group;
 
 class ConfigParser
 {
 public:
     std::unique_ptr< Config > parse( const QByteArray &content );
 
-private:
+    std::unique_ptr< Config > parse( const QDomElement &configRoot );
 
+    std::shared_ptr< Param > parseParam( const QDomElement &paramNode );
+
+    std::shared_ptr< Group > parseGroup( const QDomElement &groupNode );
 };
-
 
 }
