7504b371a76076d73f7ad330f8f222c400fe2d2f
varunamachi <varunamachi@gmail.com>
2018-07-19 18:44:54 +0530

    WIP - http parser wrapper

--- exp/http/HttpParser.cpp
+++ exp/http/HttpParser.cpp
@@ -1,6 +1,8 @@
 
 #include <cstring>
 
+#include <QByteArray>
+
 #include "HttpParser.h"
 #include "http_parser.h"
 
@@ -45,25 +47,32 @@ static int onBody(http_parser* /*p*/, const char* /*at*/, size_t /*length*/) {
     return 0;
 }
 
-static int onMessageComplete(http_parser* /*p*/) {
+static int onMessageComplete(http_parser* p) {
     //    auto data = static_cast<HttpParser *>(p->data);
     return 0;
 }
 
 
 struct HttpParser::Data {
+    Data(HttpParser::Type type)
+        : m_type(type)
+    {
+
+    }
+
     http_parser m_impl;
 
     http_parser_settings m_settings;
 
+    HttpParser::Type m_type;
+
 };
 
 HttpParser::HttpParser(Type type)
+    : m_data(std::make_unique<Data>(type))
 {
-    m_data->m_impl.data  = this;
     http_parser_init(&m_data->m_impl,
                      static_cast<http_parser_type>(type));
-
     std::memset(&m_data->m_settings, 0, sizeof(http_parser_settings));
     m_data->m_settings.on_message_begin    = onMessageBegin;
     m_data->m_settings.on_url              = onUrl;
@@ -73,7 +82,6 @@ HttpParser::HttpParser(Type type)
     m_data->m_settings.on_headers_complete = onHeadersComplete;
     m_data->m_settings.on_body             = onBody;
     m_data->m_settings.on_message_complete = onMessageComplete;
-
 }
 
 HttpParser::~HttpParser()
@@ -81,16 +89,19 @@ HttpParser::~HttpParser()
 
 }
 
-std::unique_ptr<HttpRequest> HttpParser::parseRequest(QByteArray &/*data*/)
+bool HttpParser::execute(QByteArray &data)
 {
-    return nullptr;
+    if (m_data->m_type == ParseRequest) {
+
+    } else {
+
+    }
+    auto result = http_parser_execute(
+                &m_data->m_impl,
+                &m_data->m_settings,
+                data.data(),
+                static_cast<std::size_t>(data.length())) == 0;
+    return result;
 }
-
-std::unique_ptr<HttpResponse> HttpParser::parserResponse(QByteArray &/*data*/)
-{
-    return nullptr;
-}
-
-
 
 } }
