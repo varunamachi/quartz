e16a2b00013ed0ea322979a573508fe5b84555b4
varunamachi <varunamachi@gmail.com>
2019-02-06 19:38:24 +0530

    Minor changes

--- source/core/app_config/ConfigManager.cpp
+++ source/core/app_config/ConfigManager.cpp
@@ -10,6 +10,68 @@ namespace Quartz {
 class ConfigManager::Impl
 {
 public:
+    //    AbstractConfigLoader * configLoader()
+    //    {
+    //        return m_configLoader.get();
+    //    }
+    //    const QVariant retrieve(const QString &domain,
+    //                            const QString &key) const
+    //    {
+    //        auto var = cachedValue(domain, key);
+    //        if (var.isValid()) {
+    //            return var;
+    //        }
+    //        else {
+    //            auto raw = m_storage->retrieve(domain, key);
+    //            if (! (raw.isNull() || raw.isEmpty())) {
+    //                QDataStream stream(raw);
+    //                stream.setVersion(QDataStream::Qt_5_2);
+    //                QVariant val;
+    //                stream >> val;
+    //                if (val.isValid()) {
+    //                    insertToCache(domain, key, val);
+    //                    return val;
+    //                }
+    //            }
+    //        }
+    //        return QVariant{};
+    //    }
+    //    void  store(const QString &domain,
+    //                 const QString &key,
+    //                 const QVariant &value)
+    //    {
+    //        QByteArray raw{};
+    //        QDataStream stream{ &raw, QIODevice::WriteOnly };
+    //        stream.setVersion(QDataStream::Qt_5_2);
+    //        stream << value;
+    //        if (m_storage->store(domain, key, raw)) {
+    //            insertToCache(domain, key, value);
+    //        }
+    //    }
+    //    bool has(const QString &domain,
+    //              const QString &key) const
+    //    {
+    //        bool has = cacheContains(domain, key);
+    //        if (! has) {
+    //            auto var = retrieve(domain, key);
+    //            has = var.isValid();
+    //        }
+    //        return has;
+    //    }
+    //    void remove(const QString &domain, const QString &key)
+    //    {
+    //        if (m_storage->remove(domain, key)) {
+    //            m_cache[ domain ].remove(key);
+    //        }
+    //    }
+    //    void clearCache()
+    //    {
+    //        m_cache.clear();
+    //    }
+    //private:
+
+
+public:
     explicit Impl(std::unique_ptr<IConfigStorageStrategy> storage,
                    std::unique_ptr<AbstractConfigLoader> configLoader)
         : m_storage(std::move(storage))
@@ -18,73 +80,6 @@ public:
 
     }
 
-    AbstractConfigLoader * configLoader()
-    {
-        return m_configLoader.get();
-    }
-
-    const QVariant retrieve(const QString &domain,
-                            const QString &key) const
-    {
-        auto var = cachedValue(domain, key);
-        if (var.isValid()) {
-            return var;
-        }
-        else {
-            auto raw = m_storage->retrieve(domain, key);
-            if (! (raw.isNull() || raw.isEmpty())) {
-                QDataStream stream(raw);
-                stream.setVersion(QDataStream::Qt_5_2);
-                QVariant val;
-                stream >> val;
-                if (val.isValid()) {
-                    insertToCache(domain, key, val);
-                    return val;
-                }
-            }
-        }
-        return QVariant{};
-    }
-
-    void  store(const QString &domain,
-                 const QString &key,
-                 const QVariant &value)
-    {
-        QByteArray raw{};
-        QDataStream stream{ &raw, QIODevice::WriteOnly };
-        stream.setVersion(QDataStream::Qt_5_2);
-        stream << value;
-        if (m_storage->store(domain, key, raw)) {
-            insertToCache(domain, key, value);
-        }
-    }
-
-    bool has(const QString &domain,
-              const QString &key) const
-    {
-        bool has = cacheContains(domain, key);
-        if (! has) {
-            auto var = retrieve(domain, key);
-            has = var.isValid();
-        }
-        return has;
-    }
-
-    void remove(const QString &domain, const QString &key)
-    {
-        if (m_storage->remove(domain, key)) {
-            m_cache[ domain ].remove(key);
-        }
-    }
-
-    void clearCache()
-    {
-        m_cache.clear();
-    }
-
-
-
-private:
     bool cacheContains(const QString &domain, const QString key) const
     {
         bool result = m_cache.contains(domain)
@@ -112,8 +107,7 @@ private:
         }
     }
 
-    using Cache = QHash< QString,
-                         QHash< QString, QVariant >>;
+    using Cache = QHash< QString, QHash< QString, QVariant >>;
 
     std::unique_ptr<IConfigStorageStrategy> m_storage;
 
@@ -124,6 +118,36 @@ private:
 
 const QVariant dummy;
 
+
+//void  ConfigManager::clearCache()
+//{
+//    m_impl->clearCache();
+//}
+//void  ConfigManager::store(const QString &key,
+//                            const QVariant &variant,
+//                            const QString &domain)
+//{
+//    return m_impl->store(domain, key, variant);
+//}
+//const QVariant ConfigManager::retrieve(const QString &key,
+//                                        const QString &domain) const
+//{
+//    return m_impl->retrieve(domain, key);
+//}
+//bool  ConfigManager::has(const QString &key, const QString &domain) const
+//{
+//    return m_impl->has(domain, key);
+//}
+//void ConfigManager::remove(const QString &key, const QString &domain)
+//{
+//    m_impl->remove(domain, key);
+//}
+//void  ConfigManager::batchLoad(const QByteArray &content)
+//{
+//    if (m_impl->configLoader() != nullptr) {
+//        m_impl->configLoader()->load(content);
+//    }
+//}
 
 ConfigManager::ConfigManager(
         std::unique_ptr<IConfigStorageStrategy> storageStragy,
@@ -139,46 +163,71 @@ ConfigManager::~ConfigManager()
 
 }
 
-
-void  ConfigManager::clearCache()
-{
-    m_impl->clearCache();
-}
-
-
-void  ConfigManager::store(const QString &key,
-                            const QVariant &variant,
-                            const QString &domain)
-{
-    return m_impl->store(domain, key, variant);
-}
-
 const QVariant ConfigManager::retrieve(const QString &key,
-                                        const QString &domain) const
+                                       const QString &domain) const
 {
-    return m_impl->retrieve(domain, key);
+    auto var = m_impl->cachedValue(domain, key);
+    if (var.isValid()) {
+        return var;
+    }
+    else {
+        auto raw = m_impl->m_storage->retrieve(domain, key);
+        if (! (raw.isNull() || raw.isEmpty())) {
+            QDataStream stream(raw);
+            stream.setVersion(QDataStream::Qt_5_2);
+            QVariant val;
+            stream >> val;
+            if (val.isValid()) {
+                m_impl->insertToCache(domain, key, val);
+                return val;
+            }
+        }
+    }
+    return QVariant{};
 }
 
-
-bool  ConfigManager::has(const QString &key, const QString &domain) const
+void  ConfigManager::store(
+             const QString &key,
+             const QVariant &value,
+             const QString &domain)
 {
-    return m_impl->has(domain, key);
-}
-
-
-void ConfigManager::remove(const QString &key, const QString &domain)
-{
-    m_impl->remove(domain, key);
-}
-
-
-void  ConfigManager::batchLoad(const QByteArray &content)
-{
-    if (m_impl->configLoader() != nullptr) {
-        m_impl->configLoader()->load(content);
+    QByteArray raw{};
+    QDataStream stream{ &raw, QIODevice::WriteOnly };
+    stream.setVersion(QDataStream::Qt_5_2);
+    stream << value;
+    if (m_impl->m_storage->store(domain, key, raw)) {
+        m_impl->insertToCache(domain, key, value);
     }
 }
 
+bool ConfigManager::has(const QString &key, const QString &domain) const
+{
+    bool has = m_impl->cacheContains(domain, key);
+    if (! has) {
+        auto var = retrieve(domain, key);
+        has = var.isValid();
+    }
+    return has;
+}
+
+void ConfigManager::remove(const QString &key, const QString &domain)
+{
+    if (m_impl->m_storage->remove(domain, key)) {
+        m_impl->m_cache[ domain ].remove(key);
+    }
+}
+
+void ConfigManager::clearCache()
+{
+    m_impl->m_cache.clear();
+}
+
+void  ConfigManager::batchLoad(const QByteArray &content)
+{
+    if (m_impl->m_configLoader != nullptr) {
+        m_impl->m_configLoader->load(content);
+    }
+}
 
 
 }
